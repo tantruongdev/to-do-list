115d8798efd057761cb8250f3ab60756a401b06f
Tan Truong <tanfctk@gmail.com>
2024-04-26 17:44:24 +0700

    config db

--- todo-list/src/utils/request.js
+++ todo-list/src/utils/request.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = "http://localhost:3002/";
+const API_DOMAIN = "https://todolist-h8k0.onrender.com/";
 
 export const get = async (path) => {
   const response = await fetch(API_DOMAIN + path);
