9356a2408078b25409db7df99a5c3248602d978d
Tan Truong <tanfctk@gmail.com>
2024-04-27 11:34:16 +0700

    add the alert for waiting

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,6 +1,12 @@
 import Todos from "./components/Todos";
-
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    alert(
+      "If you can't see the data, please wait a few minutes for the server to come back online. Thank you."
+    );
+  }, []);
+
   return (
     <>
       <Todos></Todos>
